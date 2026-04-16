bf00c768a675384fd4fe2dd5d2d0da4d7cb5db44
orian055 <orian050835@gmail.com>
2026-04-16 14:27:04 +0300

    initial commit

---
+++ reasons.cs
@@ -0,0 +1,13 @@
+using System.IO;
+
+public static class ReasonsPage
+{
+    public static void MapReasonsPage(this WebApplication app)
+    {
+        app.MapGet("/reasons", () =>
+        {
+            var html = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "reasons.html"));
+            return Results.Content(html, "text/html");
+        });
+    }
+}
